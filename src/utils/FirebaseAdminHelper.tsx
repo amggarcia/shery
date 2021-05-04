e0b7487dad8523dfc41779c4709f8b24e69eb954
unknown <amg.garcia@hotmail.com>
2021-05-04 21:37:43 +0100

    key parsing fix

--- src/utils/FirebaseAdminHelper.tsx
+++ src/utils/FirebaseAdminHelper.tsx
@@ -1,7 +1,10 @@
 import admin from "firebase-admin";
 const firebaseAdminConfig = {
   projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  privateKey: JSON.parse(process.env.FIREBASE_PRIVATE_KEY).replace(
+    /\\n/g,
+    "\n"
+  ),
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 };
 
